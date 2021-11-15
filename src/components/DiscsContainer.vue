843aed8f55bb22eee53c52548877518d66cc381b
Alessandro-Fedele <al.fedele@outlook.it>
2021-11-15 17:47:12 +0100

    Bonus

--- src/components/DiscsContainer.vue
+++ src/components/DiscsContainer.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="row row-cols-5 g-4">
-    <div class="col" v-for="(disc, i) in discList" :key="i">
-      <SingleDisc
-        :poster="disc.poster"
-        :title="disc.title"
-        :author="disc.author"
-        :genre="disc.genre"
-        :year="disc.year"
-      ></SingleDisc>
+  <div>
+    <div class="row row-cols-5 g-4">
+      <div class="col" v-for="(disc, i) in discList" :key="i">
+        <SingleDisc
+          :poster="disc.poster"
+          :title="disc.title"
+          :author="disc.author"
+          :genre="disc.genre"
+          :year="disc.year"
+        ></SingleDisc>
+      </div>
     </div>
+    <Loader label="Pacman" v-if="loading === true"></Loader>
   </div>
 </template>
 <script>
 import axios from "axios";
 import SingleDisc from "./SingleDisc.vue";
+import Loader from "./Loader.vue";
 export default {
   name: "DiscsContainer",
-  components: { SingleDisc },
+  components: { SingleDisc, Loader },
   data() {
     return {
       discList: [],
+      loading: true,
     };
   },
   mounted() {
@@ -27,6 +32,9 @@ export default {
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((resp) => {
         this.discList = resp.data.response;
+        setTimeout(() => {
+          this.loading = false;
+        }, 1500);
       });
   },
 };
