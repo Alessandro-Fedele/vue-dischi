4497f3fa5b953ead39e06905209f895a0e63b2a6
Alessandro-Fedele <al.fedele@outlook.it>
2021-11-16 19:21:51 +0100

    Filter for Genre

--- src/components/DiscsContainer.vue
+++ src/components/DiscsContainer.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <SelectGenre @genreClick="genreFilter"></SelectGenre>
     <div class="row row-cols-5 g-4">
-      <div class="col" v-for="(disc, i) in discList" :key="i">
+      <div class="col" v-for="(disc, i) in filteredGenreCom" :key="i">
         <SingleDisc
           :poster="disc.poster"
           :title="disc.title"
@@ -18,14 +19,21 @@
 import axios from "axios";
 import SingleDisc from "./SingleDisc.vue";
 import Loader from "./Loader.vue";
+import SelectGenre from "./SelectGenre.vue";
 export default {
   name: "DiscsContainer",
-  components: { SingleDisc, Loader },
+  components: { SingleDisc, Loader, SelectGenre },
   data() {
     return {
       discList: [],
       loading: true,
+      filteredGenre: "",
     };
+  },
+  methods: {
+    genreFilter(genreToFilter) {
+      this.filteredGenre = genreToFilter;
+    },
   },
   mounted() {
     axios
@@ -36,6 +44,17 @@ export default {
           this.loading = false;
         }, 1500);
       });
+  },
+  computed: {
+    // Ogni volta che la variabile filteredGenre cambia grazie all'utente la computed si aggiorna
+    filteredGenreCom() {
+      if (!this.filteredGenre) {
+        return this.discList;
+      }
+      return this.discList.filter((disc) => {
+        return this.filteredGenre === disc.genre.toLowerCase();
+      });
+    },
   },
 };
 </script>
---
+++ src/components/SelectGenre.vue
@@ -0,0 +1,23 @@
+<template>
+  <select
+    v-model="genreValue"
+    class="form-select"
+    @change="$emit(`genreClick`, genreValue)"
+  >
+    <option value="">Tutti i Generi</option>
+    <option value="rock">Rock</option>
+    <option value="pop">Pop</option>
+    <option value="jazz">Jazz</option>
+    <option value="metal">Metal</option>
+  </select>
+</template>
+<script>
+export default {
+  name: "SelectGenre",
+  data() {
+    return {
+      genreValue: "",
+    };
+  },
+};
+</script>
